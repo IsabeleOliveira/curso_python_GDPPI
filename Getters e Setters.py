c06133fe359e97dc508e610b19f93b40a364e41f
Isa_oliveira <belefreiresls@outlook.com>
2021-12-26 17:56:54 -0300

    Update Getters e Setters.py

--- Getters e Setters.py
+++ Getters e Setters.py
@@ -1,8 +1,35 @@
+# SETTER = CONFIGURANDO UM VALOR (=), PRECISA TER UM GETTER PRIMEIRO
+# GETTER = RETORNA O VALOR CONFIGURADO NO SETTER E PDE TER ELE SEM SETTER (.)
+
+class Pessoa:
+    def __init__(self, nome):
+        self._nome = nome
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
+    @property
+    def sobrenome(self):
+        return 'Oliveira'
+
+
+
+p1 = Pessoa('Isabele')
+print(p1.nome)
+print(p1.sobrenome)
+
+"""
+
 
 class Produto:
     def __init__(self, nome, preco):
-        self.nome = nome
-        self.preco = preco
+        self._nome = nome
+        self._preco = preco
 
     def desconto(self, percentual):
         self.preco = self.preco - (self.preco * (percentual/100))
@@ -13,7 +40,7 @@ class Produto:
 
     @nome.setter
     def nome(self, valor):
-        self.nome = valor
+        self._nome = valor
 
     #Getter
     @property
@@ -28,3 +55,4 @@ class Produto:
             valor = valor.replace('R$', '')
 
         self.preco = valor
+"""
